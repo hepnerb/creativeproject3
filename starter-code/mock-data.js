60744103c34d0ff3c9e4f0d902a612e229b8ae70
hepnerb <hepnerbryce@gmail.com>
2020-11-07 20:32:35 -0700

    commit the images

--- starter-code/mock-data.js
+++ starter-code/mock-data.js
@@ -1,358 +1,354 @@
 let mock = [{
-    id: 1,
-    name: "Egg Salad",
-    price: "$5.62",
-    country: "United States",
-    image: 'egg-salad.jpg'
-  },
-  {
-    id: 2,
-    name: "Sweet Potato",
-    price: "$2.41",
-    country: "Brazil",
-    image: "sweet-potato.jpg"
-  },
-  {
-    id: 3,
-    name: "Foccacia Bread",
-    price: "$5.92",
-    country: "United States",
-    image: 'focaccia.jpg'
-  },
-  {
-    id: 4,
-    name: "Canned Lotus Rootlets",
-    price: "$3.58",
-    country: "United States",
-    image: 'lotus-rootlets.jpg'
-  },
-  {
-    id: 5,
-    name: "Frilled Toothpicks",
-    price: "$2.47",
-    country: "Canada",
-    image: 'toothpicks.jpg'
+  "id": 1,
+  "make": "Dodge",
+  "model": "D350",
+  "year": 1992,
+  "price": "$5949.92",
+  "image": "1.jpg"
+}, {
+  "id": 2,
+  "make": "Lexus",
+  "model": "IS",
+  "year": 2011,
+  "price": "$7721.50",
+  "image": "2.jpg"
+}, {
+  "id": 3,
+  "make": "Toyota",
+  "model": "Camry",
+  "year": 2009,
+  "price": "$8483.65",
+  "image": "3.jpg"
+}, {
+  "id": 4,
+  "make": "Geo",
+  "model": "Tracker",
+  "year": 1993,
+  "price": "$3455.80",
+  "image": "4.jpg"
+}, {
+  "id": 5,
+  "make": "Honda",
+  "model": "Odyssey",
+  "year": 2001,
+  "price": "$9913.27",
+  "image": "5.jpg"
+}, {
+  "id": 6,
+  "make": "Dodge",
+  "model": "Ram Van 1500",
+  "year": 2000,
+  "price": "$5306.75",
+  "image": "6.jpg"
+}, {
+  "id": 7,
+  "make": "Audi",
+  "model": "5000S",
+  "year": 1988,
+  "price": "$5151.63",
+  "image": "7.jpg"
+}, {
+  "id": 8,
+  "make": "Lamborghini",
+  "model": "Diablo",
+  "year": 1994,
+  "price": "$7172.64",
+  "image": "8.jpg"
+}, {
+  "id": 9,
+  "make": "Mitsubishi",
+  "model": "Expo",
+  "year": 1994,
+  "price": "$6570.74",
+  "image": "9.jpg"
+}, {
+  "id": 10,
+  "make": "Ford",
+  "model": "Windstar",
+  "year": 2002,
+  "price": "$8256.60",
+  "image": "11.jpg"
+}, {
+  "id": 11,
+  "make": "Volkswagen",
+  "model": "Routan",
+  "year": 2009,
+  "price": "$7926.52",
+  "image": "11.jpg"
+}, {
+  "id": 12,
+  "make": "Subaru",
+  "model": "Impreza",
+  "year": 2000,
+  "price": "$5616.58",
+  "image": "12.jpg"
+}, {
+  "id": 13,
+  "make": "Buick",
+  "model": "LeSabre",
+  "year": 1993,
+  "price": "$3146.11",
+  "image": "13.jpg"
+}, {
+  "id": 14,
+  "make": "Kia",
+  "model": "Optima",
+  "year": 2003,
+  "price": "$2853.50",
+  "image": "14.jpg"
+}, {
+  "id": 15,
+  "make": "Land Rover",
+  "model": "Freelander",
+  "year": 2002,
+  "price": "$5757.50",
+  "image": "15.jpg"
+}, {
+  "id": 16,
+  "make": "Tesla",
+  "model": "Roadster",
+  "year": 2012,
+  "price": "$12008.82",
+  "image": "16.jpg"
+}, {
+  "id": 17,
+  "make": "GMC",
+  "model": "Savana",
+  "year": 2009,
+  "price": "$6046.52",
+  "image": "17.jpg"
+}, {
+  "id": 18,
+  "make": "Chevrolet",
+  "model": "Silverado 3500",
+  "year": 2001,
+  "price": "$5038.69",
+  "image": "18.jpg"
+}, {
+  "id": 19,
+  "make": "Jeep",
+  "model": "Commander",
+  "year": 2010,
+  "price": "$3987.05",
+  "image": "19.jpg"
+}, {
+  "id": 20,
+  "make": "Ford",
+  "model": "Mustang",
+  "year": 1987,
+  "price": "$6689.53",
+  "image": "20.jpg"
+}, {
+  "id": 21,
+  "make": "Mitsubishi",
+  "model": "Montero",
+  "year": 2002,
+  "price": "$4516.47",
+  "image": "21.jpg"
+}, {
+  "id": 22,
+  "make": "Maserati",
+  "model": "GranSport",
+  "year": 2006,
+  "price": "$18785.72",
+  "image": "22.jpg"
+}, {
+  "id": 23,
+  "make": "Mercedes-Benz",
+  "model": "SL-Class",
+  "year": 1995,
+  "price": "$3533.32",
+  "image": "23.jpg"
+}, {
+  "id": 24,
+  "make": "Mercury",
+  "model": "Montego",
+  "year": 2007,
+  "price": "$2946.39",
+  "image": "24.jpg"
+}, {
+  "id": 25,
+  "make": "Ford",
+  "model": "Mustang",
+  "year": 1965,
+  "price": "$17548.67",
+  "image": "25.jpg"
+}, {
+  "id": 26,
+  "make": "Lincoln",
+  "model": "MKZ",
+  "year": 2008,
+  "price": "$5956.87",
+  "image": "26.jpg"
+}, {
+  "id": 27,
+  "make": "Suzuki",
+  "model": "Aerio",
+  "year": 2003,
+  "price": "$2062.82",
+  "image": "27.jpg"
+}, {
+  "id": 28,
+  "make": "Cadillac",
+  "model": "Fleetwood",
+  "year": 1995,
+  "price": "$3142.82",
+  "image": "28.jpg"
+}, {
+  "id": 29,
+  "make": "Volkswagen",
+  "model": "Jetta",
+  "year": 1984,
+  "price": "$9265.33",
+  "image": "29.jpg"
+}, {
+  "id": 30,
+  "make": "GMC",
+  "model": "Suburban 1500",
+  "year": 1997,
+  "price": "$3695.62",
+  "image": "30.jpg"
+}, {
+  "id": 31,
+  "make": "Porsche",
+  "model": "911",
+  "year": 2012,
+  "price": "$20595.38",
+  "image": "31.jpg"
+}, {
+  "id": 32,
+  "make": "Ford",
+  "model": "LTD Crown Victoria",
+  "year": 1991,
+  "price": "$9603.73",
+  "image": "32.jpg"
+}, {
+  "id": 33,
+  "make": "Mazda",
+  "model": "MX-5",
+  "year": 2009,
+  "price": "$2100.16",
+  "image": "33.jpg"
+}, {
+  "id": 34,
+  "make": "Ford",
+  "model": "Aspire",
+  "year": 1997,
+  "price": "$3313.35",
+  "image": "34.jpg"
+}, {
+  "id": 35,
+  "make": "Toyota",
+  "model": "Tercel",
+  "year": 1993,
+  "price": "$8079.46",
+  "image": "35.jpg"
+}, {
+  "id": 36,
+  "make": "Ford",
+  "model": "Taurus",
+  "year": 2000,
+  "price": "$5337.04",
+  "image": "36.jpg"
+}, {
+  "id": 37,
+  "make": "Oldsmobile",
+  "model": "Silhouette",
+  "year": 2002,
+  "price": "$3566.38",
+  "image": "37.jpg"
+}, {
+  "id": 38,
+  "make": "Chevrolet",
+  "model": "Camaro",
+  "year": 1972,
+  "price": "$19511.65",
+  "image": "38.jpg"
+}, {
+  "id": 39,
+  "make": "Nissan",
+  "model": "Sentra",
+  "year": 1998,
+  "price": "$4999.24",
+  "image": "39.jpg"
+}, {
+  "id": 40,
+  "make": "Volvo",
+  "model": "S80",
+  "year": 2002,
+  "price": "$6033.12",
+  "image": "40.jpg"
+}, {
+  "id": 41,
+  "make": "Honda",
+  "model": "Odyssey",
+  "year": 2005,
+  "price": "$9364.76",
+  "image": "41.jpg"
+}, {
+  "id": 42,
+  "make": "Lexus",
+  "model": "RX Hybrid",
+  "year": 2007,
+  "price": "$4432.87",
+  "image": "42.jpg"
+}, {
+  "id": 43,
+  "make": "Buick",
+  "model": "Estate",
+  "year": 1990,
+  "price": "$4261.86",
+  "image": "43.jpg"
+}, {
+  "id": 44,
+  "make": "Hyundai",
+  "model": "Tucson",
+  "year": 2005,
+  "price": "$4551.29",
+  "image": "44.jpg"
+}, {
+  "id": 45,
+  "make": "Mitsubishi",
+  "model": "Diamante",
+  "year": 1996,
+  "price": "$9603.79",
+  "image": "45.jpg"
+}, {
+  "id": 46,
+  "make": "BMW",
+  "model": "6 Series",
+  "year": 2006,
+  "price": "$4570.46",
+  "image": "46.jpg"
+}, {
+  "id": 47,
+  "make": "Dodge",
+  "model": "Challenger",
+  "year": 2008,
+  "price": "$12670.76",
+  "image": "47.jpg"
+}, {
+  "id": 48,
+  "make": "Ford",
+  "model": "Escape",
+  "year": 2006,
+  "price": "$8732.52",
+  "image": "48.jpg"
+}, {
+  "id": 49,
+  "make": "Dodge",
+  "model": "Spirit",
+  "year": 1995,
+  "price": "$9930.27",
+  "image": "49.jpg"
+}, {
+  "id": 50,
+  "make": "Mitsubishi",
+  "model": "Galant",
+  "year": 1996,
+  "price": "$7054.72",
+  "image": "50.jpg"
+}]
 
-  },
-  {
-    id: 6,
-    name: "Rubbed Sage",
-    price: "$8.77",
-    country: "Brazil",
-    image: 'sage-rubbed.jpg'
-  },
-  {
-    id: 7,
-    name: "Langers Mango Nectar",
-    price: "$9.24",
-    country: "Brazil",
-    image: 'mango-nectar.jpg'
-  },
-  {
-    id: 8,
-    name: "Cucumber",
-    price: "$3.02",
-    country: "Canada",
-    image: 'cucumber.jpg'
-  },
-  {
-    id: 9,
-    name: "Fava Beans",
-    price: "$4.76",
-    country: "Brazil",
-    image: 'fava-beans.jpg'
-
-  },
-  {
-    id: 10,
-    name: "Coconut",
-    price: "$2.50",
-    country: "Brazil",
-    image: 'coconut.jpg'
-  },
-  {
-    id: 11,
-    name: "Kettle Potato Chips",
-    price: "$6.70",
-    country: "United States",
-    image: "potato-chips.jpg"
-  },
-  {
-    id: 12,
-    name: "Flour - Rye",
-    price: "$1.14",
-    country: "Canada",
-    image: "rye-flour.jpg"
-  },
-  {
-    id: 13,
-    name: "Feta Chees",
-    price: "$6.92",
-    country: "Brazil",
-    image: "feta-cheese.jpg"
-  },
-  {
-    id: 14,
-    name: "Cheddar Cheese",
-    price: "$5.36",
-    country: "Mexico",
-    image: "cheddar-cheese.jpg"
-  },
-  {
-    id: 15,
-    name: "Capers",
-    price: "$4.18",
-    country: "United States",
-    image: "capers.jpg"
-  },
-  {
-    id: 16,
-    name: "Baguette",
-    price: "$9.18",
-    country: "Mexico",
-    image: "baguette.jpg"
-  },
-  {
-    id: 17,
-    name: "Lasagna Noodles",
-    price: "$4.73",
-    country: "Brazil",
-    image: "lasagna-pasta.jpg"
-  },
-  {
-    id: 18,
-    name: "Pied De Vents Cheese",
-    price: "$5.16",
-    country: "Canada",
-    image: "pied-de-vents.jpg"
-  },
-  {
-    id: 19,
-    name: "Sweet Potato Crackers",
-    price: "$6.81",
-    country: "United States",
-    image: "crackers.jpg"
-  },
-  {
-    id: 20,
-    name: "Mushrooms",
-    price: "$8.62",
-    country: "United States",
-    image: "mushrooms.jpg"
-  },
-  {
-    id: 21,
-    name: "Jerusalem Artichoke",
-    price: "$3.16",
-    country: "United States",
-    image: "jerusalem-artichoke.jpg"
-  },
-  {
-    id: 22,
-    name: "Leather Leaf Fern",
-    price: "$9.41",
-    country: "Brazil",
-    image: "leather-leaf.jpg"
-  },
-  {
-    id: 23,
-    name: "Dried Apricots",
-    price: "$7.42",
-    country: "Brazil",
-    image: "dried-apricots.jpg"
-  },
-  {
-    id: 24,
-    name: "Navel Orange",
-    price: "$3.53",
-    country: "Brazil",
-    image: "navel-orange.jpg"
-  },
-  {
-    id: 25,
-    name: "Sunflower Oil",
-    price: "$1.90",
-    country: "Mexico",
-    image: "sunflower-oil.jpg"
-  },
-  {
-    id: 26,
-    name: "Black Turtle Beans",
-    price: "$4.67",
-    country: "Canada",
-    image: "turtle-beans.jpg"
-  },
-  {
-    id: 27,
-    name: "Bananas",
-    price: "$2.64",
-    country: "Canada",
-    image: "bananas.jpg"
-
-  },
-  {
-    id: 28,
-    name: "Salami",
-    price: "$8.72",
-    country: "Brazil",
-    image: "salami.jpg"
-  },
-  {
-    id: 29,
-    name: "Proscuitto",
-    price: "$9.86",
-    country: "Brazil",
-    image: "proscuitto.jpg"
-  },
-  {
-    id: 30,
-    name: "Cranberries",
-    price: "$5.85",
-    country: "United States",
-    image: "cranberries.jpg"
-
-  },
-  {
-    id: 31,
-    name: "Canteloupe",
-    price: "$4.49",
-    country: "United States",
-    image: "canteloupe.jpg"
-  },
-  {
-    id: 32,
-    name: "Lemon",
-    price: "$2.8Lemon5",
-    country: "United States",
-    image: "lemon.jpg"
-  },
-  {
-    id: 33,
-    name: "Pesto",
-    price: "$2.67",
-    country: "Brazil",
-    image: "pesto.jpg"
-  },
-  {
-    id: 34,
-    name: "Grapefruit Juice",
-    price: "$4.88",
-    country: "Brazil",
-    image: "grapefruit-juice.jpg"
-
-  },
-  {
-    id: 35,
-    name: "Curry Paste",
-    price: "$2.02",
-    country: "Brazil",
-    image: "curry-paste.jpg"
-  },
-  {
-    id: 36,
-    name: "Avocado Oil",
-    price: "$4.45",
-    country: "Canada",
-    image: "avocado-oil.jpg"
-  },
-  {
-    id: 37,
-    name: "Queso Fresco",
-    price: "$9.24",
-    country: "Canada",
-    image: "queso-fresco.jpg"
-  },
-  {
-    id: 38,
-    name: "Key Lime Pie",
-    price: "$5.32",
-    country: "Mexico",
-    image: "key-lime-pie.jpg"
-  },
-  {
-    id: 39,
-    name: "Granny Smith Apple",
-    price: "$2.12",
-    country: "United States",
-    image: "granny-smith.jpg"
-  },
-  {
-    id: 40,
-    name: "Morning Glory Muffin Mix",
-    price: "$9.22",
-    country: "Brazil",
-    image: "muffin-mix.jpg"
-  },
-  {
-    id: 41,
-    name: "Heavy Whipping Cream",
-    price: "$8.78",
-    country: "Brazil",
-    image: 'heavy-cream.jpg'
-  },
-  {
-    id: 42,
-    name: "Red Bell Pepper",
-    price: "$9.03",
-    country: "Brazil",
-    image: 'red-bell-pepper.jpg'
-  },
-  {
-    id: 43,
-    name: "Perrier",
-    price: "$9.36",
-    country: "United States",
-    image: 'perrier.jpg'
-  },
-  {
-    id: 44,
-    name: "Yukon Gold Potatoes",
-    price: "$8.76",
-    country: "Mexico",
-    image: 'yukon-potatoes.jpg'
-  },
-  {
-    id: 45,
-    name: "Salmon Fillets",
-    price: "$6.21",
-    country: "United States",
-    image: 'salmon-fillets.jpg'
-  },
-  {
-    id: 46,
-    name: "Tilapia Fillets",
-    price: "$4.37",
-    country: "Mexico",
-    image: 'tilapia.jpg'
-  },
-  {
-    id: 47,
-    name: "Tomato Pesto",
-    price: "$9.70",
-    country: "Brazil",
-    image: 'tomato-pesto.jpg'
-  },
-  {
-    id: 48,
-    name: "Oil - Olive Bertolli",
-    price: "$8.92",
-    country: "Brazil",
-    image: 'olive-oil.jpg'
-  },
-  {
-    id: 49,
-    name: "Tear Drop Yellow Tomatoes",
-    price: "$3.32",
-    country: "Mexico",
-    image: 'yellow-tomatoes.jpg'
-  },
-  {
-    id: 50,
-    name: "Smoked Paprika",
-    price: "$8.31",
-    country: "Brazil",
-    image: 'smoked-paprika.jpg'
-  }
-]
 
 export default mock;
